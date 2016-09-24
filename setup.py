3d36bb8cf9727fa9093aea8c1aa24e8372a0af90
John Garcia <john.garcia@bons.ai>
2016-09-23 21:04:43 -0700

    Eighth try at fixing deps

--- setup.py
+++ setup.py
@@ -31,11 +31,11 @@ setup(
     install_requires=[
         'websockets>=3.1,<4',
         'protobuf>=3.0.0,<4',
-        'bonsai-config==0.2.0',
+        'bonsai_config==0.2.0',
     ],
     dependency_links=[
         # Temporary until we get bonsai-config on PyPI
-        'http://github.com/BonsaiAI/bonsai-config/tarball/master#egg=bonsai-config-0.2.0',
+        'git+https://github.com/BonsaiAI/bonsai-config.git#egg=bonsai_config-0.2.0',
     ],
     packages=find_packages()
     )
